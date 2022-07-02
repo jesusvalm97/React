ce81bbde232c9b191140412f7aed9bff3be6cb90
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-07-02 07:35:37 -0500

    Ejemplos de uso de map y filter

--- Fundamentos de javascript/Fundamentos.js
+++ Fundamentos de javascript/Fundamentos.js
@@ -1,10 +1,10 @@
 //fetch
-fetch("https://pokeapi.co/api/v2/pokemon")
-  .then((resp) => resp.json())
-  .then((data) => {
-    //console.log(data.results.map(pokemon => pokemon.name));
-  })
-  .catch((error) => console.log(error));
+//fetch("https://pokeapi.co/api/v2/pokemon")
+//  .then((resp) => resp.json())
+//  .then((data) => {
+//console.log(data.results.map(pokemon => pokemon.name));
+//  })
+//  .catch((error) => console.log(error));
 
 //async await
 const ObtenerPokemones = async () => {
@@ -12,12 +12,27 @@ const ObtenerPokemones = async () => {
     const resp = await fetch("https://pokeapi.co/api/v2/pokemon");
     const data = await resp.json();
 
-    console.log(data);
+    //console.log(data.results);
 
+    /*
+    map nos sirve para indicar a un arreglo o lista que nos
+    devuelva solo un arreglo de la propiedad que le indiquemos
+    // de cada objeto del arreglo/lista original
+    */
+    console.log(data.results.map((pokemon) => pokemon.name));
 
+    /*
+    filter nos sirve para indicar a un arreglo o lista que nos
+    devuelva solo un arreglo de la propiedad que le indiquemos
+    de cada objeto del arreglo/lista original pero también
+    podemos filtrar el resultado, por ejemplo en este caso
+    indicamos que nos traiga los nombres de los pokemones,
+    de ese resultado que solo traiga los que el nombres sea
+    igual a bulbasaur
+    */
+    console.log(data.results.filter((pokemon) => pokemon.name !== "bulbasaur"));
   } catch (error) {
     console.log(error);
   }
 };
-
 ObtenerPokemones();
