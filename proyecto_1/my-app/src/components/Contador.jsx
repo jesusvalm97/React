eceabab70f35d76e098bc11e4ec810223116a6dc
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-07-04 11:02:18 -0500

    Update Contador.jsx

--- proyecto_1/my-app/src/components/Contador.jsx
+++ proyecto_1/my-app/src/components/Contador.jsx
@@ -12,7 +12,7 @@ export const Contador = () => {
   return (
     <div>
         <h1>Contador</h1>
-        <h3>Nuertro número aumentando: { contador }</h3>
+        <h3>Nuestro número aumentando: { contador }</h3>
         {/* <button onClick={ () => aumentar() }>Aumentar</button> */}
         <button onClick={ () => setContador(contador + 1) }>Aumentar</button>
     </div>
