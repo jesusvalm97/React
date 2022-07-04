243296c64ef867f0a423e786e38d083fe6d3a6fd
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-07-04 11:05:00 -0500

    Uso del operador ternario
    Uso del operador ternario para mostrar un valor

--- proyecto_1/my-app/src/components/Contador.jsx
+++ proyecto_1/my-app/src/components/Contador.jsx
@@ -13,6 +13,11 @@ export const Contador = () => {
     <div>
         <h1>Contador</h1>
         <h3>Nuestro número aumentando: { contador }</h3>
+        <h4>
+            {
+                contador > 2 ? 'Es mayor a dos' : 'Es menor a dos'
+            }
+        </h4>
         {/* <button onClick={ () => aumentar() }>Aumentar</button> */}
         <button onClick={ () => setContador(contador + 1) }>Aumentar</button>
     </div>
