a4e6237f25c80d9ec35b43cc022e54680a9ed377
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-07-30 12:43:37 -0500

    create the menu component

---
+++ Curriculum/jesus-antero-varela-almanza-cv/src/App.jsx
@@ -0,0 +1,11 @@
+import { Menu } from "./components/Menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ Curriculum/jesus-antero-varela-almanza-cv/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <ul>
+        <li><a href=''>Perfil</a></li>
+        <li><a href=''>Softskills</a></li>
+        <li><a href=''>Hardskills</a></li>
+        <li><a href=''>Experiencia</a></li>
+        <li><a href=''>Educaci&oacute;n</a></li>
+        <li><a href=''>Hobbies</a></li>
+        <li><a href=''>Contacto</a></li>
+    </ul>
+  )
+}
