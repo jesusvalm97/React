5ecd5746e438a6253a84250d89b3025d75a2400c
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-07-30 12:49:10 -0500

    Create [erfil component

--- Curriculum/jesus-antero-varela-almanza-cv/src/App.jsx
+++ Curriculum/jesus-antero-varela-almanza-cv/src/App.jsx
@@ -1,9 +1,12 @@
 import { Menu } from "./components/Menu";
+import { Perfil } from "./components/Perfil";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <h1>JES&Uacute;S ANTERO VARELA ALMANZA</h1>
+      <Perfil />
     </div>
   );
 }
---
+++ Curriculum/jesus-antero-varela-almanza-cv/src/components/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Perfil = () => {
+  return (
+    <div>
+        <h2>PERFIL</h2>
+        <p>
+            Desarrollador de software. Trabajador persistente, orientado al detalle y autodidacta. Habilidades interpersonales que me hacen un gran trabajador en equipo. En busca de aprendizaje y desarrollo laboral.
+        </p>
+    </div>
+  )
+}
